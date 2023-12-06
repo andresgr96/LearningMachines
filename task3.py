5ee5b1b3d51917cf100870788b61a318e4c7c093
andres <andresgarciar96@gmail.com>
2023-12-06 14:39:03 +0100

    minor setup update

--- task3.py
+++ task3.py
@@ -13,35 +13,37 @@ def terminate_program(signal_number, frame):
     print("Ctrl-C received, terminating program")
     sys.exit(1)
 
+
 def main():
     signal.signal(signal.SIGINT, terminate_program)
     # rob = robobo.HardwareRobobo(camera=True).connect(address="192.168.1.7")
 
     rob = robobo.SimulationRobobo().connect(address='127.0.0.1', port=19997)
 
-
     moving = True
     turning = False
-
 
     rob.play_simulation()
 
     while True:
-        center_sen = np.log(rob.read_irs()[5]) / 10
-        back_sen = np.log(rob.read_irs()[1]) / 10
-
-        # print("ROB Irs: {}".format(np.log(np.array(rob.read_irs())) / 10))
-        print("Center Sensor: " + str(center_sen))
-        print("Back Sensor: " + str(back_sen))
+        try:
+            center_sen = np.log(rob.read_irs()[5]) / 10
+            back_sen = np.log(rob.read_irs()[1]) / 10
+            print("Center Sensor: " + str(center_sen))
+            print("Back Sensor: " + str(back_sen))
+        except:
+            print(f"Error reading proximity sensors")
+            time.sleep(0.1)
+            continue
 
         if moving:
             # print("robobo is at {}".format(rob.position()))
             rob.move(8, 8, 2000)
             print("State: Moving")
 
-            # if -100 < center_sen <= -0.17:
-            #     moving = False
-            #     turning = True
+            if -100 < center_sen <= -0.17:
+                moving = False
+                turning = True
 
         elif turning:
             rob.move(-8, 8, 2000)
@@ -50,21 +52,21 @@ def main():
                 moving = True
                 turning = False
 
-# Following code gets an image from the camera
+    # Following code gets an image from the camera
     image = rob.get_image_front()
     # IMPORTANT! `image` returned by the simulator is BGR, not RGB
-    cv2.imwrite("results/test_pictures.png", image)
+    cv2.imwrite("imgs/test_pictures.png", image)
 
     time.sleep(0.1)
 
     # IR reading
     for i in range(10000):
-        print("ROB Irs: {}".format(np.log(np.array(rob.read_irs()))/10))
+        print("ROB Irs: {}".format(np.log(np.array(rob.read_irs())) / 10))
         time.sleep(0.1)
 
     # pause the simulation and read the collected food
     rob.pause_simulation()
-    
+
     # Stopping the simualtion resets the environment
     rob.stop_world()
 
